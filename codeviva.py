de68550bb20f90c9640a861f8c07ee6daebb54d4
rajdeep02-23 <102215655+rajdeep02-23@users.noreply.github.com>
2022-04-13 14:11:37 +0530

    Add files via upload

---
+++ codeviva.py
@@ -0,0 +1,12 @@
+<html>
+<body>
+<button type="button" onclick="abc()"> Hello Change the text</button>
+<p id="pqr"> Hello i am dimple</p>
+<script>
+function abc()
+{
+document.getElementById('pqr').style.color='red';
+}
+</script>
+</body>
+</html>
